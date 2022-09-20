cabf33245479e2ab65d7ef03ff1f0de0ed16dff4
JuanSuarezFSD <juanjesussuarezs@gmail.com>
2022-09-20 11:15:44 +0100

    Update main.php

--- main.php
+++ main.php
@@ -1,51 +1,57 @@
 <?php
-    $horario= array(
-        "Lunes"=>array(1=>array("modulo"=>"EMR","nombre"=>"Empresa e iniciativa emprendedora","docente"=>"María del Sol García Tarajano","aula"=>"201","hora"=>"1ª"),
-                        2=>array("modulo"=>"DSW","nombre"=>"Desarrollo Web en entorno servidor","docente"=>"Sergio Ramos Suárez","aula"=>"201","hora"=>"2ª"),
-                        3=>array("modulo"=>"DSW","nombre"=>"Desarrollo Web en entorno servidor","docente"=>"Sergio Ramos Suárez","aula"=>"201","hora"=>"3ª"),
-                        4=>array("modulo"=>"DEW","nombre"=>"Desarrollo Web en entorno cliente","docente"=>"María del Carmen Rodríguez Suárez","aula"=>"201","hora"=>"4ª"),
-                        5=>array("modulo"=>"DEW","nombre"=>"Desarrollo Web en entorno cliente","docente"=>"María del Carmen Rodríguez Suárez","aula"=>"201","hora"=>"5ª"),
-                        6=>array("modulo"=>"DEW","nombre"=>"Desarrollo Web en entorno cliente","docente"=>"María del Carmen Rodríguez Suárez","aula"=>"201","hora"=>"6ª"),
+    $Horario= array(
+        "Lunes"=>array(1=>array("Nombre"=>"Empresa e iniciativa emprendedora","Docente"=>"María del Sol García Tarajano","Aula"=>"201","Hora"=>"1ª"),
+                        2=>array("Nombre"=>"Desarrollo Web en entorno servidor","Docente"=>"Sergio Ramos Suárez","Aula"=>"201","Hora"=>"2ª"),
+                        3=>array("Nombre"=>"Desarrollo Web en entorno servidor","Docente"=>"Sergio Ramos Suárez","Aula"=>"201","Hora"=>"3ª"),
+                        4=>array("Nombre"=>"Desarrollo Web en entorno cliente","Docente"=>"María del Carmen Rodríguez Suárez","Aula"=>"201","Hora"=>"4ª"),
+                        5=>array("Nombre"=>"Desarrollo Web en entorno cliente","Docente"=>"María del Carmen Rodríguez Suárez","Aula"=>"201","Hora"=>"5ª"),
+                        6=>array("Nombre"=>"Desarrollo Web en entorno cliente","Docente"=>"María del Carmen Rodríguez Suárez","Aula"=>"201","Hora"=>"6ª"),
                     ),
-        "Martes"=>array(1=>array("modulo"=>"DPL","nombre"=>"Despliegue de aplicaciones web","docente"=>"María Antonia Montesdeoca Viera","aula"=>"201","hora"=>"1ª"),
-                        2=>array("modulo"=>"DPL","nombre"=>"Despliegue de aplicaciones web","docente"=>"María Antonia Montesdeoca Viera","aula"=>"201","hora"=>"2ª"),
-                        3=>array("modulo"=>"DSW","nombre"=>"Desarrollo Web en entorno servidor","docente"=>"Sergio Ramos Suárez","aula"=>"201","hora"=>"3ª"),
-                        4=>array("modulo"=>"DSW","nombre"=>"Desarrollo Web en entorno servidor","docente"=>"Sergio Ramos Suárez","aula"=>"201","hora"=>"4ª"),
-                        5=>array("modulo"=>"DOR","nombre"=>"Diseño de interfaces web","docente"=>"Ermis Papakonstantinou Baez","aula"=>"201","hora"=>"5ª"),
-                        6=>array("modulo"=>"DOR","nombre"=>"Diseño de interfaces web","docente"=>"Ermis Papakonstantinou Baez","aula"=>"201","hora"=>"6ª"),
+        "Martes"=>array(1=>array("Nombre"=>"Despliegue de aplicaciones web","Docente"=>"María Antonia Montesdeoca Viera","Aula"=>"201","Hora"=>"1ª"),
+                        2=>array("Nombre"=>"Despliegue de aplicaciones web","Docente"=>"María Antonia Montesdeoca Viera","Aula"=>"201","Hora"=>"2ª"),
+                        3=>array("Nombre"=>"Desarrollo Web en entorno servidor","Docente"=>"Sergio Ramos Suárez","Aula"=>"201","Hora"=>"3ª"),
+                        4=>array("Nombre"=>"Desarrollo Web en entorno servidor","Docente"=>"Sergio Ramos Suárez","Aula"=>"201","Hora"=>"4ª"),
+                        5=>array("Nombre"=>"Diseño de interfaces web","Docente"=>"Ermis Papakonstantinou Baez","Aula"=>"201","Hora"=>"5ª"),
+                        6=>array("Nombre"=>"Diseño de interfaces web","Docente"=>"Ermis Papakonstantinou Baez","Aula"=>"201","Hora"=>"6ª"),
                     ),
-        "Miércoles"=>array(1=>array("modulo"=>"DEW","nombre"=>"Desarrollo Web en entorno cliente","docente"=>"María del Carmen Rodríguez Suárez","aula"=>"201","hora"=>"1ª"),
-                        2=>array("modulo"=>"DEW","nombre"=>"Desarrollo Web en entorno cliente","docente"=>"María del Carmen Rodríguez Suárez","aula"=>"201","hora"=>"2ª"),
-                        3=>array("modulo"=>"DSW","nombre"=>"Desarrollo Web en entorno servidor","docente"=>"Sergio Ramos Suárez","aula"=>"201","hora"=>"3ª"),
-                        4=>array("modulo"=>"DSW","nombre"=>"Desarrollo Web en entorno servidor","docente"=>"Sergio Ramos Suárez","aula"=>"201","hora"=>"4ª"),
-                        5=>array("modulo"=>"DOR","nombre"=>"Diseño de interfaces web","docente"=>"Ermis Papakonstantinou Baez","aula"=>"201","hora"=>"5ª"),
-                        6=>array("modulo"=>"DOR","nombre"=>"Diseño de interfaces web","docente"=>"Ermis Papakonstantinou Baez","aula"=>"201","hora"=>"6ª"),
+        "Miércoles"=>array(1=>array("Nombre"=>"Desarrollo Web en entorno cliente","Docente"=>"María del Carmen Rodríguez Suárez","Aula"=>"201","Hora"=>"1ª"),
+                        2=>array("Nombre"=>"Desarrollo Web en entorno cliente","Docente"=>"María del Carmen Rodríguez Suárez","Aula"=>"201","Hora"=>"2ª"),
+                        3=>array("Nombre"=>"Desarrollo Web en entorno servidor","Docente"=>"Sergio Ramos Suárez","Aula"=>"201","Hora"=>"3ª"),
+                        4=>array("Nombre"=>"Desarrollo Web en entorno servidor","Docente"=>"Sergio Ramos Suárez","Aula"=>"201","Hora"=>"4ª"),
+                        5=>array("Nombre"=>"Diseño de interfaces web","Docente"=>"Ermis Papakonstantinou Baez","Aula"=>"201","Hora"=>"5ª"),
+                        6=>array("Nombre"=>"Diseño de interfaces web","Docente"=>"Ermis Papakonstantinou Baez","Aula"=>"201","Hora"=>"6ª"),
                     ),
-        "Jueves"=>array(1=>array("modulo"=>"DPL","nombre"=>"Despliegue de aplicaciones web","docente"=>"María Antonia Montesdeoca Viera","aula"=>"201","hora"=>"1ª"),
-                    2=>array("modulo"=>"DPL","nombre"=>"Despliegue de aplicaciones web","docente"=>"María Antonia Montesdeoca Viera","aula"=>"201","hora"=>"2ª"),
-                    3=>array("modulo"=>"DPL","nombre"=>"Despliegue de aplicaciones web","docente"=>"María Antonia Montesdeoca Viera","aula"=>"201","hora"=>"3ª"),
-                    4=>array("modulo"=>"DEW","nombre"=>"Desarrollo Web en entorno cliente","docente"=>"María del Carmen Rodríguez Suárez","aula"=>"201","hora"=>"4ª"),
-                    5=>array("modulo"=>"DEW","nombre"=>"Desarrollo Web en entorno cliente","docente"=>"María del Carmen Rodríguez Suárez","aula"=>"201","hora"=>"5ª"),
-                    6=>array("modulo"=>"EMR","nombre"=>"Empresa e iniciativa emprendedora","docente"=>"María del Sol García Tarajano","aula"=>"201","hora"=>"6ª"),
+        "Jueves"=>array(1=>array("Nombre"=>"Despliegue de aplicaciones web","Docente"=>"María Antonia Montesdeoca Viera","Aula"=>"201","Hora"=>"1ª"),
+                    2=>array("Nombre"=>"Despliegue de aplicaciones web","Docente"=>"María Antonia Montesdeoca Viera","Aula"=>"201","Hora"=>"2ª"),
+                    3=>array("Nombre"=>"Despliegue de aplicaciones web","Docente"=>"María Antonia Montesdeoca Viera","Aula"=>"201","Hora"=>"3ª"),
+                    4=>array("Nombre"=>"Desarrollo Web en entorno cliente","Docente"=>"María del Carmen Rodríguez Suárez","Aula"=>"201","Hora"=>"4ª"),
+                    5=>array("Nombre"=>"Desarrollo Web en entorno cliente","Docente"=>"María del Carmen Rodríguez Suárez","Aula"=>"201","Hora"=>"5ª"),
+                    6=>array("Nombre"=>"Empresa e iniciativa emprendedora","Docente"=>"María del Sol García Tarajano","Aula"=>"201","Hora"=>"6ª"),
                 ),
-        "Viernes"=>array(1=>array("modulo"=>"DOR","nombre"=>"Diseño de interfaces web","docente"=>"Ermis Papakonstantinou Baez","aula"=>"201","hora"=>"1ª"),
-                2=>array("modulo"=>"DOR","nombre"=>"Diseño de interfaces web","docente"=>"Ermis Papakonstantinou Baez","aula"=>"201","hora"=>"2ª"),
-                3=>array("modulo"=>"DPL","nombre"=>"Despliegue de aplicaciones web","docente"=>"María Antonia Montesdeoca Viera","aula"=>"201","hora"=>"3ª"),
-                4=>array("modulo"=>"EMR","nombre"=>"Empresa e iniciativa emprendedora","docente"=>"María del Sol García Tarajano","aula"=>"201","hora"=>"4ª"),
-                5=>array("modulo"=>"DSW","nombre"=>"Desarrollo Web en entorno servidor","docente"=>"Sergio Ramos Suárez","aula"=>"201","hora"=>"5ª"),
-                6=>array("modulo"=>"DSW","nombre"=>"Desarrollo Web en entorno servidor","docente"=>"Sergio Ramos Suárez","aula"=>"201","hora"=>"6ª"),
+        "Viernes"=>array(1=>array("Nombre"=>"Diseño de interfaces web","Docente"=>"Ermis Papakonstantinou Baez","Aula"=>"201","Hora"=>"1ª"),
+                2=>array("Nombre"=>"Diseño de interfaces web","Docente"=>"Ermis Papakonstantinou Baez","Aula"=>"201","Hora"=>"2ª"),
+                3=>array("Nombre"=>"Despliegue de aplicaciones web","Docente"=>"María Antonia Montesdeoca Viera","Aula"=>"201","Hora"=>"3ª"),
+                4=>array("Nombre"=>"Empresa e iniciativa emprendedora","Docente"=>"María del Sol García Tarajano","Aula"=>"201","Hora"=>"4ª"),
+                5=>array("Nombre"=>"Desarrollo Web en entorno servidor","Docente"=>"Sergio Ramos Suárez","Aula"=>"201","Hora"=>"5ª"),
+                6=>array("Nombre"=>"Desarrollo Web en entorno servidor","Docente"=>"Sergio Ramos Suárez","Aula"=>"201","Hora"=>"6ª"),
             ),
         );
 
         function mostrarcontenido(){
-            foreach($horario as $clave => $valor){
-                echo "Las asignaturas del $clave son:"
-                foreach($valor as $clave1 => $valor1)
-                    echo "A $clave1 ª hora:"
-                    foreach($valor1 as $clave11=>$valor11)
-                        echo "Tienes "
+            foreach($Horario as $clave => $valor){
+                echo "$clave : ";
+                echo "<br>";
+                foreach($valor as $clave1 => $valor1){
+                    echo "A $clave1 ª hora:";
+                    foreach($valor1 as $clave2=>$valor2){
+                        echo "<br>";
+                        echo "$clave2 : $valor2";
+                        echo "<br>";
+                    }
+                    echo "<br>";
+                    }  
+                }
             }
-        }
 
 ?>
 
