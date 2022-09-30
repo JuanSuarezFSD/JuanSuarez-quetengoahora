298c994c360d37e3c6a56de090815fe9282ea7ae
JuanSuarezFSD <juanjesussuarezs@gmail.com>
2022-09-30 13:38:25 +0100

    Update

---
+++ ClaseDocente.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Seleccione el horario que quiera ver:</h1>
+
+<form action="/action_page.php">
+  <select name="horario" id="horario">
+    <optgroup label="Grupos">
+      <option value="DAW">2 DAW M</option>
+      <option value="DAM">2 DAM M</option>
+    </optgroup>
+    <optgroup label="Docente">
+      <option value="Sergio">Sergio Ramos</option>
+      <option value="Ermis">Ermis Griego</option>
+    </optgroup>
+  </select>
+  <br><br>
+  <input type="submit" value="Submit">
+</form>
+ 
+</body>
+</html>
